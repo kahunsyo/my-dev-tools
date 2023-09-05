43e21b16352be6cbf0f02f10bf1e2ae81c920680
kahunsyo <contact2ichino@gmail.com>
2023-09-05 14:24:19 +0900

    fix: not found omf error

---
+++ install_bobthefish.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/fish
+
+# Install bobthefish
+# omf is able to run in fish
+fish -c 'omf update'
+fish -c 'omf install bobthefish'
--- install_fish_extension.sh
+++ install_fish_extension.sh
@@ -1,11 +1,9 @@
-#!/bin/fish
+#!/bin/sh
+set -e 
 
 # Install Oh My Fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install -o /tmp/install
 fish /tmp/install --noninteractive
-source ~/.config/fish/config.fish
 
 # Install bobthefish
-# omf is able to run in fish
-fish -c 'omf update'
-fish -c 'omf install bobthefish'
+fish install_bobthefish.sh
