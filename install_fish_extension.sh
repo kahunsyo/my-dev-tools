21f2436cd5e96b3295902ec9b01f404edeb33bd2
ichino <81274957+kahunsyo@users.noreply.github.com>
2023-09-05 13:11:33 +0900

    Update install_fish_extension.sh

--- install_fish_extension.sh
+++ install_fish_extension.sh
@@ -3,6 +3,7 @@
 # Install Oh My Fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install -o /tmp/install
 fish /tmp/install --noninteractive
+source ~/.config/fish/config.fish
 
 # Install bobthefish
 # omf is able to run in fish
